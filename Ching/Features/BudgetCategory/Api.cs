e4f218c9ec2033aba17c8bc01d14ba0740f791ad
Gerrie Crafford <10078179+GerrieCrafford@users.noreply.github.com>
2023-02-08 07:06:31 +0200

    add: fluent validation

--- Ching/Features/BudgetCategory/Api.cs
+++ Ching/Features/BudgetCategory/Api.cs
@@ -8,13 +8,13 @@ public static class BudgetCategoriesEndpoints
 {
     public static void MapBudgetCategoriesApi(this RouteGroupBuilder group)
     {
-        group.MapPost("/", Create)
+        group.MapPost("/", CreateHandler)
             .Produces<int>()
             .Produces<List<FluentValidation.Results.ValidationFailure>>(400)
             .WithName("CreateBudgetCategory");
     }
 
-    public static async Task<IResult> Create(CreateBudgetCategoryRequest request, IMediator mediator, IMapper mapper, CreateBudgetCategoryRequestValidator validator)
+    public static async Task<IResult> CreateHandler(Create.Command request, IMediator mediator, IMapper mapper, Create.CommandValidator validator)
     {
         var result = await validator.ValidateAsync(request);
         if (!result.IsValid)
